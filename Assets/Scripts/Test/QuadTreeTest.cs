3a65336e4773495218e2298467ce6c4829bc0ec7
Wonderland6627 <994015020@qq.com>
2021-03-12 20:42:33 +0800

    Init Commit

---
+++ Assets/Scripts/Test/QuadTreeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+using QuadTree;
+
+public class QuadTreeTest : MonoBehaviour
+{
+    private QuadTree<GameObject> quadTree;
+
+    private void Start()
+    {
+        
+    }
+
+    private void TestMethod()
+    {
+       
+    }
+}
